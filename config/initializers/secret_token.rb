4dd50161a1c2922d54810ad4703a4b818551a36c
Jasmine Chabra <jchabra@gmail.com>
2012-12-12 11:44:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wishez::Application.config.secret_token = 'a1529f1e42e3924ee311d92845b1894dc3b305506ac85749ad8f4bfc42c09482fa7ff5c0a556e01191964e9b4b674a09de55e3c94436182a58728a57d99d86f8'
